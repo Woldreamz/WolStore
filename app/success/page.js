eeab5f548fe3d6cdeea52dff05b725a707608def
Rainer Nsa <woldreamz@gmail.com>
2024-04-05 10:38:19 +0100

    God is my King

--- app/success/page.js
+++ app/success/page.js
@@ -2,8 +2,8 @@ import Link from "next/link";
 
 export default function SuccessPage() {
     return (
-        <div className="sticky top-0 p-6">
-            Hooray! <br />
+        <div className="flex flex-col p-6 flex-1 items-center justify-center pb-10 md:pb-14">
+            Hooray! 
             <Link href={'/'}>Back home</Link>
         </div>
     )
