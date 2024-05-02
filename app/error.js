af944aa2839394216b9e3e0fdf22280873b0c0e1
Rainer Nsa <woldreamz@gmail.com>
2024-05-02 18:58:00 +0100

    City of God

--- app/error.js
+++ app/error.js
@@ -1,6 +1,8 @@
 "use client"
 export default function Error() {
     return (
-        <div>Ewooo..... ee don sup</div>
+        <div className="flex flex-col p-6 flex-1 items-center justify-center pb-10 md:pb-14">
+            Ewooo..... ee don sup
+        </div>
     )
 }
