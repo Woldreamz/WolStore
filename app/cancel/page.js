08a601b9877f8cbec3746e5b173d242de852fd88
Rainer Nsa <woldreamz@gmail.com>
2024-05-03 17:20:43 +0100

    City of God

--- app/cancel/page.js
+++ app/cancel/page.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function CancelPage() {
     return (
         <div>
-            Gutted!!
+            Dang!!
             <Link>Back home</Link>
         </div>
     )
